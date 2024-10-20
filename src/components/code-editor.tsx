e76ebe50aa49945ca88def98d1d21db1b07fe1c0
vijay <VijayIyer>
2024-10-20 17:19:25 -0400

    Changes till video 145

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,11 @@
 import {useRef} from 'react';
+import './code-editor.css';
+import './syntax.css';
 import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
 import prettier from 'prettier';
 import parser from 'prettier/parser-babel';
+import codeShift from 'jscodeshift';
+import Highlighter from 'monaco-jsx-highlighter';
 
 interface CodeEditorProps {
     initialValue: string;
@@ -18,20 +22,44 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
         });
 
         monacoEditor.getModel()?.updateOptions({ tabSize: 2});
+
+        const highlighter = new Highlighter(
+            //@ts-ignore
+            window.monaco,
+            codeShift,
+            monacoEditor
+        );
+        highlighter.highLightOnDidChangeModelContent(
+            () => {},
+            () => {},
+            undefined,
+            () => {}
+        );
     };
 
     const onFormatClick = () => {
         console.log(editorRef.current)
         // get current value from editor
-        
+        const unformatted = editorRef.current.getModel().getValue();
         // format the value
-
+        const formatted = prettier.format(unformatted, {
+            parser: 'babel',
+            plugins: [parser],
+            useTabs: false,
+            semi: true,
+            singleQuote: true
+        }).replace(/\n$/, '');
         // set the formatted value back in the editor
+        editorRef.current.setValue(formatted);
     }
 
     return (
-        <div>
-            <button onClick={onFormatClick}>Format</button>
+        <div className='editor-wrapper'>
+            <button 
+                className="button button-format is-primary is-small" 
+                onClick={onFormatClick}>
+                    Format
+            </button>
             <MonacoEditor
                 editorDidMount={onEditorMount}
                 value={initialValue}
